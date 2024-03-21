0525263777c58a69c32cfdd37cae4f1b1e379d67
VlShost <vlshost9386@gmail.com>
2024-03-21 11:57:21 +0200

    add section components and css files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import Hero from '../components/Hero';
+import About from '../components/About';
+import MindMap from '../components/MindMap';
+import Faq from '../components/Faq';
+import Arts from '../components/Arts';
+import ContactUs from '../components/ContactUs';
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <About />
+      <MindMap />
+      <Faq />
+      <Arts />
+      <ContactUs />
     </>
   );
 };
