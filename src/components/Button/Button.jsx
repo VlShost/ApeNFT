98204623c3dbed533b35d1d16a44fbb883cff209
VlShost <vlshost9386@gmail.com>
2024-03-22 18:16:51 +0200

    add btn component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import css from './Button.module.css';
+
+const Button = ({ children, section }) => {
+  console.log(section);
+  return (
+    <button
+      className={`${css.btn} ${
+        (section === 'hero' && css.heroBtn) || (section === 'form' && css.formBtn)
+      }`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
