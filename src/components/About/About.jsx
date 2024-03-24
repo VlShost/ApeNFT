40419774950f5f21ce422ba9d119750d0c95c3cd
VlShost <vlshost9386@gmail.com>
2024-03-24 12:06:32 +0200

    add icon sprite and banner animation

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,6 +6,7 @@ import ape_tabl1x from '../../assets/images/about/ape_tabl@1x.png';
 import ape_tabl2x from '../../assets/images/about/ape_tabl@2x.png';
 import ape_desk1x from '../../assets/images/about/ape_desk@1x.png';
 import ape_desk2x from '../../assets/images/about/ape_desk@2x.png';
+import icons from '../../assets/images/icons.svg';
 
 import css from './About.module.css';
 
@@ -20,7 +21,11 @@ const About = () => {
           WHO GOT FED UP WITH BORING AND HYPOCRITIC COMMONPLACE THIS IS HOW THE IDEA OF ESCAPING AND
           DYNAMIC JOURNEY ON THE YACHT
         </p>
+
         <p className={css.txtLower}>
+          <svg className={css.icon} width="24px" height="24px">
+            <use href={icons + '#cross'}></use>
+          </svg>
           EACH ARTWORK IN THE COLLECTION TELLS A STORY OF ONE JOURNEY. ARTS ARE RANDOMLY GENERATED
           BY USERS
         </p>
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,14 +1,31 @@
+import icons from '../../assets/images/icons.svg';
+
 import css from './Banner.module.css';
 
 const Banner = () => {
   return (
     <div className={css.banner}>
       <div className={css.txtScroll}>
-        <p className={css.txt}>Destroy stereotypes</p>
+        <p className={css.txt}>
+          Destroy stereotypes
+          <svg className={css.icon} width="36px" height="36px">
+            <use href={icons + '#cross'}></use>
+          </svg>
+        </p>
 
-        <p className={css.txt}>HAVE NO LIMITS</p>
+        <p className={css.txt}>
+          HAVE NO LIMITS
+          <svg className={css.icon} width="36px" height="36px">
+            <use href={icons + '#cross'}></use>
+          </svg>
+        </p>
 
-        <p className={css.txt}>Break rules</p>
+        <p className={css.txt}>
+          Break rules
+          <svg className={css.icon} width="36px" height="36px">
+            <use href={icons + '#cross'}></use>
+          </svg>
+        </p>
       </div>
     </div>
   );
