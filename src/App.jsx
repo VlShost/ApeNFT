ac6d149cbfa0f50a4b212086ed7b0a3c7644e992
VlShost <vlshost9386@gmail.com>
2024-03-25 22:19:39 +0200

    404 fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import Home from './pages/Home';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/ApeNFT/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="*" element={<Navigate to={'/'} />} />
+        <Route path="*" element={<Navigate to={'/ApeNFT/'} />} />
       </Route>
     </Routes>
   );
